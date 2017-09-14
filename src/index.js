1410fc6f920231792e1c090f58d7f24ddce89144
Gabriel Leventhal <gabriel@leventhal.ch>
2017-09-14 16:12:42 -0400

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // var d3 = require('d3');
+var R = require('randgen');
 var pd = require('probability-distributions');
 var _ = require('lodash');
 var Promise = require("bluebird");
@@ -13,26 +14,47 @@ var d = 0.0;   // dispersal
 var b1 = 0.0027;
 var b2 = -0.005;
 
+function binom(n, p, lim = 30.0) {
+  if (n <= 0.0) {
+    return 0.0;
+  } else if (p >= 1.0) {
+    return n;
+  } else if (p <= 0) {
+    return 0.0;
+  } else {
+    let mean = n*p;
+    if (mean > lim) {
+      let sigma = Math.sqrt(mean*(1-d));
+      let x = Math.floor(R.rnorm(mean,sigma));
+      x = (x < 0) ? 0 : x;
+      x = (x > n) ? n : x;
+      return x;
+    } else {
+      // console.log([n,p])
+      let x = pd.rbinom(1, n, p);
+      return x[0];
+    }
+  }
+}
+
 function next_generation(n1, n2) {
   let n = Math.floor(r*n1);
   let p = Math.exp(b1*n1 + b2*n2);
   p = p > 1.0 ? 1.0 : p;
-  return pd.rbinom(1, n, p);
+  return binom(n,p,100);
 }
 
 function disperse(data) {
+  //console.log(d);
   let i = data[0].length-1;
   let N = 0;
   let q = 0;
   for (let j = 0; j < m; ++j) {
-    if (d < 1.0) {
-      q = pd.rbinom(1, data[j][i], d)[0];
-    } else {
-      q = data[j][i];
-    }
+    q = binom(data[j][i],d,30.0);
     data[j][i] -= q;
     N += q;
   }
+  //console.log(N);
   if (N > 0) {
     let dist = pd.sample(_.range(m), N, true);
     _.each(dist, function(j) { data[j][i]++ });
@@ -58,6 +80,13 @@ var graphOpts = {
     animationDuration: 0, // duration of animations when hovering an item
   },
   responsiveAnimationDuration: 0, // animation duration after a resize
+  scales: {
+    yAxes: [{
+      id: 'left-y-axis',
+      type: 'logarithmic',
+      position: 'left'
+    }]
+  }
 }
 
 var loaderStr = '<div class="loader" id="graph"></div>';
@@ -75,31 +104,33 @@ var go = function() {
       b1 = $('#b1').val();
       b2 = $('#b2').val();
 
+      d = parseInt($('#d').val());
+
       var x = [ 0, 1 ];
-      var y0 = [ $('#y0').val(), $('#y1').val() ];
-      var data = [];
+      var y0 = [ parseInt($('#y0').val()), parseInt($('#y1').val()) ];
+
+      let data = [];
       for (let i = 0; i < m; i++) {
         data[i] = y0.slice();
       }
 
       for (let i = 2; i <= gen; i++) {
-        console.log(i);
         x.push(i);
         for (let j = 0; j < m; j++) {
-          let n2 = next_generation(data[j][i-1], data[j][i-2]);
-          data[j][i] = n2[0];
-          if (n2[0] > 10000) {
-            reject("Overflow");
-          }
+          // console.log([i, j, data[j][i-1], data[j][i-2]]);
+          var n2 = next_generation(data[j][i-1], data[j][i-2]);
+          // console.log(n2);
+          data[j][i] = n2;
+          // if (n2[0] > 10000) { break; }
         }
         disperse(data);
       }
 
-      var datasets = [];
+      let datasets = [];
       for (let j = 0; j < m; j++) {
         datasets[j] = {
           label: 'Patch ' + j,
-          data: data[j],
+          data: data[j].slice(),
           fill: false,
           borderColor: colors[j]
         };
